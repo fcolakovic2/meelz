ef40548679c34384be0e384ebf58cd65b38d1936
Faris Čolaković <fcolakovic2@etf.unsa.ba>
2021-05-17 14:48:12 +0200

    dodan responsive layout ovisno o postojanju slike

--- lib/kartica.dart
+++ lib/kartica.dart
@@ -13,47 +13,82 @@ class _OrderKarticaState extends State<OrderKartica> {
   @override
   Widget build(BuildContext context) {
     SizeConfig().init(context);
-
+    bool imaSlike = true;
+    if (widget.image == "") {
+      imaSlike = false;
+    }
     return Center(
       child: Card(
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.circular(15.0),
         ),
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: <Widget>[
-            Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: <Widget>[
-                Text(
-                  widget.title,
-                ),
-              ],
-            ),
-            SizedBox(
-              height: SizeConfig.safeBlockVertical * 2,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: <Widget>[
-                Text(
-                  widget.subtitle,
-                ),
-              ],
-            ),
-            SizedBox(
-              height: SizeConfig.safeBlockVertical * 2,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: <Widget>[
-                Text(
-                  widget.cijena,
-                ),
-              ],
-            )
-          ],
-        ),
+        child: imaSlike
+            ? Column(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: <Widget>[
+                      Text(
+                        widget.title,
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: SizeConfig.safeBlockVertical * 2,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: <Widget>[
+                      Text(
+                        widget.subtitle,
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: SizeConfig.safeBlockVertical * 2,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: <Widget>[
+                      Text(
+                        widget.cijena,
+                      ),
+                    ],
+                  )
+                ],
+              )
+            : Column(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: <Widget>[
+                      Column(children: <Widget>[
+                        Image.asset(widget.image),
+                      ]),
+                      Column(children: <Widget>[
+                        Text(
+                          widget.title,
+                        ),
+                        SizedBox(height: SizeConfig.safeBlockVertical * 2),
+                        Text(widget.subtitle),
+                      ]),
+                    ],
+                  ),
+                  SizedBox(
+                    height: SizeConfig.safeBlockVertical * 2,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: <Widget>[
+                      Text(
+                        widget.cijena,
+                      ),
+                    ],
+                  )
+                ],
+              ),
       ),
     );
   }
