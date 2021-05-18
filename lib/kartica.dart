48dc1d215ce91b4a709d6df29d37408f6ef116c1
Faris Čolaković <fcolakovic2@etf.unsa.ba>
2021-05-18 14:22:59 +0200

    Dodana promjena shapea carda ovisno o slici

--- lib/kartica.dart
+++ lib/kartica.dart
@@ -18,9 +18,11 @@ class _OrderKarticaState extends State<OrderKartica> {
 
     return Center(
       child: Card(
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(15.0),
-        ),
+        shape: !widget.imaSlike
+            ? RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(15.0),
+              )
+            : RoundedRectangleBorder(),
         child: widget.imaSlike
             ? Column(
                 mainAxisSize: MainAxisSize.min,
@@ -32,24 +34,27 @@ class _OrderKarticaState extends State<OrderKartica> {
                         Container(
                           height: SizeConfig.safeBlockVertical * 15,
                           width: SizeConfig.safeBlockHorizontal * 15,
-                          child: Image.network(
-                              'https://github.com/flutter/plugins/raw/master/packages/video_player/video_player/doc/demo_ipod.gif?raw=true'),
-                        )
+                          child: Image.asset('assets/images/Rectangle711.png'),
+                        ),
                       ]),
                       Column(children: <Widget>[
-                        Text(
-                          widget.title,
-                          style: TextStyle(
-                            fontSize: 16,
-                            fontStyle: FontStyle.normal,
-                            fontWeight: FontWeight.w500,
-                            color: Color(0xff373737),
+                        Padding(
+                          padding: const EdgeInsets.only(left: 8.0),
+                          child: Text(
+                            widget.title,
+                            style: TextStyle(
+                              fontFamily: "Inter",
+                              fontSize: 16,
+                              fontStyle: FontStyle.normal,
+                              fontWeight: FontWeight.w500,
+                              color: Color(0xff373737),
+                            ),
                           ),
                         ),
-                        SizedBox(height: SizeConfig.safeBlockVertical * 2),
                         Text(
                           widget.subtitle,
                           style: TextStyle(
+                            fontFamily: "Inter",
                             fontSize: 14,
                             fontStyle: FontStyle.normal,
                             fontWeight: FontWeight.w500,
@@ -59,152 +64,138 @@ class _OrderKarticaState extends State<OrderKartica> {
                       ]),
                     ],
                   ),
-                  SizedBox(
-                    height: SizeConfig.safeBlockVertical * 2,
-                  ),
-                  widget.imaSlike
-                      ? Row(
-                          mainAxisAlignment: !widget.imaSlike
-                              ? MainAxisAlignment.end
-                              : MainAxisAlignment.start,
-                          children: <Widget>[
-                            Column(children: <Widget>[
-                              Text(
-                                widget.cijena,
-                              ),
-                            ]),
-                          ],
-                        )
-                      : Row(
-                          mainAxisAlignment: !widget.imaSlike
-                              ? MainAxisAlignment.end
-                              : MainAxisAlignment.start,
-                          children: <Widget>[
-                            Column(children: <Widget>[
-                              Text(
-                                widget.cijena,
-                                style: TextStyle(
-                                  fontSize: 16,
-                                  fontStyle: FontStyle.normal,
-                                  fontWeight: FontWeight.w500,
-                                  color: Color(0x373737),
-                                ),
-                              ),
-                            ]),
-                          ],
-                        )
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: <Widget>[
+                      Column(children: <Widget>[
+                        Padding(
+                          padding:
+                              const EdgeInsets.only(left: 78.0, bottom: 20.0),
+                          child: Text(
+                            widget.cijena,
+                          ),
+                        ),
+                      ]),
+                    ],
+                  )
                 ],
               )
             : Column(
                 mainAxisSize: MainAxisSize.min,
                 children: <Widget>[
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: <Widget>[
-                      Column(
-                        children: [
-                          Text(
-                            widget.title,
-                            style: TextStyle(
-                              fontSize: 16,
-                              fontStyle: FontStyle.normal,
-                              fontWeight: FontWeight.w500,
-                              color: Color(0xff373737),
-                            ),
-                          ),
-                        ],
-                      ),
-                      Column(
-                        children: <Widget>[
-                          SizedBox(width: SizeConfig.screenWidth * 0.65)
-                        ],
-                      ),
-                      Column(
-                        children: <Widget>[Icon(Icons.more_vert)],
-                      ),
-                    ],
-                  ),
-                  SizedBox(
-                    height: SizeConfig.safeBlockVertical * 2,
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: <Widget>[
-                      Text(
-                        widget.subtitle,
-                        style: TextStyle(
-                          fontSize: 14,
-                          fontStyle: FontStyle.normal,
-                          fontWeight: FontWeight.w500,
-                          color: Color(0xff373737).withOpacity(0.6),
-                        ),
-                      ),
-                    ],
-                  ),
-                  SizedBox(
-                    //ovdje dodaj sad i pending i delivery date, a dole obrisi pending i delivery dodaj sliku samo. zato se ne vide pending i delivery jer otvori uvijek ovo ako je slika true tj ako postoji
-                    height: SizeConfig.safeBlockVertical * 2,
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: <Widget>[
-                      Column(
-                        children: <Widget>[
-                          Chip(
-                            backgroundColor: Colors.yellow,
-                            label: Text(
-                              "Pending",
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(20.0, 20.0, 20.0, 8.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        Column(
+                          children: [
+                            Text(
+                              widget.title,
                               style: TextStyle(
-                                fontSize: 12,
-                                fontStyle: FontStyle.normal,
-                                fontWeight: FontWeight.w500,
-                                color: Color(0xff68572D),
-                              ),
-                            ),
-                          )
-                        ],
-                      ),
-                      Column(
-                        children: <Widget>[
-                          SizedBox(width: SizeConfig.safeBlockHorizontal * 3)
-                        ],
-                      ),
-                      Column(
-                        children: <Widget>[
-                          Chip(
-                            backgroundColor: Color(0xffF6F6F6),
-                            label: Text(
-                              "Feb 13 delivery",
-                              style: TextStyle(
-                                fontSize: 12,
+                                fontSize: 16,
+                                fontFamily: "Inter",
                                 fontStyle: FontStyle.normal,
                                 fontWeight: FontWeight.w500,
                                 color: Color(0xff373737),
                               ),
                             ),
-                          )
-                        ],
-                      ),
-                      Column(
-                        children: <Widget>[
-                          SizedBox(width: SizeConfig.safeBlockHorizontal * 3)
-                        ],
-                      ),
-                      Column(
-                        children: [
-                          Text(
-                            widget.cijena,
-                            style: TextStyle(
-                              fontSize: 16,
-                              fontStyle: FontStyle.normal,
-                              fontWeight: FontWeight.w500,
-                              color: Color(0x373737),
-                            ),
+                          ],
+                        ),
+                        Column(
+                          children: <Widget>[
+                            Icon(Icons.more_vert),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: <Widget>[
+                      Padding(
+                        padding:
+                            const EdgeInsets.only(left: 20.0, bottom: 10.0),
+                        child: Text(
+                          widget.subtitle,
+                          style: TextStyle(
+                            fontSize: 14,
+                            fontFamily: "Inter",
+                            fontStyle: FontStyle.normal,
+                            fontWeight: FontWeight.w500,
+                            color: Color(0xff373737).withOpacity(0.6),
                           ),
-                        ],
+                        ),
                       ),
                     ],
-                  )
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(18.0, 10.0, 20.0, 20),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: <Widget>[
+                        Padding(
+                          padding: const EdgeInsets.only(right: 5.0),
+                          child: Column(
+                            children: <Widget>[
+                              Chip(
+                                shape: RoundedRectangleBorder(
+                                    borderRadius:
+                                        BorderRadius.all(Radius.circular(6))),
+                                backgroundColor: Color(0xffFFDF36),
+                                label: Text(
+                                  "Pending",
+                                  style: TextStyle(
+                                    fontFamily: "Inter",
+                                    fontSize: 12,
+                                    fontStyle: FontStyle.normal,
+                                    fontWeight: FontWeight.w500,
+                                    color: Color(0xff68572D),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                        Column(
+                          children: <Widget>[
+                            Chip(
+                              shape: RoundedRectangleBorder(
+                                  borderRadius:
+                                      BorderRadius.all(Radius.circular(6))),
+                              backgroundColor: Color(0xffF6F6F6),
+                              label: Text(
+                                "Feb 13 delivery",
+                                style: TextStyle(
+                                  fontFamily: "Inter",
+                                  fontSize: 12,
+                                  fontStyle: FontStyle.normal,
+                                  fontWeight: FontWeight.w500,
+                                  color: Color(0xff373737),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                        Spacer(),
+                        Column(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            Text(
+                              widget.cijena,
+                              style: TextStyle(
+                                fontSize: 16,
+                                fontFamily: "Inter",
+                                fontStyle: FontStyle.normal,
+                                fontWeight: FontWeight.w500,
+                                color: Color(0xff373737),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
                 ],
               ),
       ),
